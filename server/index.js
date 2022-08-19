8a3b4ef28fde8a751c1d9aa628cad308db9367c5
Syt <3295539817@qq.com>
2022-08-20 00:40:22 +0800

    init

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const port = 2233;
+
+httpServer.listen(port, () => {
+  console.log('正在监听端口：' + port);
+});
+
+const io = new Server(httpServer, {
+  cors: {
+    // origin: "https://example.com",
+    allowedHeaders: ["*"],
+    credentials: true
+  }
+});
+io.on('connection', (socket) => {
+
+  const remoteUser = socket.request.connection.remoteAddress + ':' + socket.request.connection.remotePort;
+
+  console.log('来自' + remoteUser + '的新连接');
+
+  socket.on('disconnect', function () {
+    console.log('用户' + remoteUser + '断开连接');
+  });
+
+  socket.on('video-control', (controlParam) => {
+    console.log('用户' + remoteUser + '的消息:' + controlParam);
+    io.emit('video-control', controlParam);
+  });
+
+});
