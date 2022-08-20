72f01f6a1f1ac1ba008e18bae5b86c8dbf25bd48
Syt <3295539817@qq.com>
2022-08-20 22:56:41 +0800

    优化socket.io接口，使用本地开发服务器代理socket.io

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
+  publicPath: './',
+  devServer: {
+    port: 8080,
+    proxy: {
+      '/': {
+        target: 'http://localhost:2233/',
+        ws: false
+        // 将请求前缀key（key为正则表达式）重写为value。用来将请求前缀统一替换
+        // pathRewrite: { 'key': 'value' }
+      }
+    }
+  }
 })
