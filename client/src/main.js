0fd09c09286758096ccd643c4e696cbdfbbb02e7
Syt <3295539817@qq.com>
2022-08-30 00:15:20 +0800

    添加normalize.css

--- client/src/main.js
+++ client/src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'normalize.css'
+
 import VueAliplayerV2 from 'vue-aliplayer-v2'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/display.css'
 
 Vue.use(VueAliplayerV2)
 Vue.use(ElementUI)
